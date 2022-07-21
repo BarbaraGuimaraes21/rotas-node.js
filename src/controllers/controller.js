b09fb34c718f889169c6d27253b9136b33a437ec
Barbara Guimaraes <bahcamargo21@gmail.com>
2022-07-20 21:36:35 -0300

    criado código js controller

---
+++ src/controllers/controller.js
@@ -0,0 +1,16 @@
+import Dica from "../models/models"
+
+const dicaController = (app) => {
+    app.get('/dica',(req,res) => {
+        const dica = new Dica ()
+        res.json({"Dicas": dica.pegaDica()})
+    })
+
+    app.post('/dica',(req,res) => {
+        const dica = new Dica(req.body.dica)
+        dica.insereDica(dica)
+        res.json({"Dicas":dica})
+    })
+}
+
+export default dicaController
